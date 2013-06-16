71a23f84d8b54b29ed002aea2036f13e2517c334
Vandana <vandana@vandana-lab.(none)>
2013-06-16 02:33:12 -0500

    created package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="AccessTwitterAPI",
+    version='0.1.0',
+    author='Vandana Bachani',
+    author_email='vandana.bvj@gmail.com',
+    packages=['src'],
+    scripts=['src/ata.py'],
+    url='https://github.com/vandanab/ata',
+    license='MIT License',
+    description='Simplified wrapper for Twitter REST API calls',
+    long_description=open('README.txt').read(),
+)
